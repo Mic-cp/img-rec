1ad16d9a61332cab5eb78be60f031603fb0a117a
Mic-cp <83549796+Mic-cp@users.noreply.github.com>
2021-11-02 20:55:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 //https://teachablemachine.withgoogle.com/models/5SDOpF8w4/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera")
+
+Webcam.attach('#camera')
+
+
+function take_snapshot()
+{
+   
+    Webcam.snap(function(data_uri) {
+       document.getElementById("result").innerHTML='<img id=captured_image" src="'+data_uri+'"/>'
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5SDOpF8w4/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log("modelLoaded")
+}
